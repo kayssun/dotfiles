12481fc84aa55790581d4a3cbf365ff8c19d033a
Gerrit Visscher <gerrit@visscher.de>
2015-05-12 21:41:05 +0200

    Add git shortcuts

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,8 @@
 set PATH ~/.bin /usr/local/sbin $PATH
+# Git shortcuts
+# update local repo even with local changes
+alias ks-update "git stash; and git pull --rebase; and git push; and git stash apply"
+# update release branch
+alias ks-release "git checkout release; and git merge master; and git push; and git checkout master"
+# update preview branch
+alias ks-preview "git checkout preview; and git merge master; and git push; and git checkout master"
