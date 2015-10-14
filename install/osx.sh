60d0aa6f74446c3c89329cb8017bbb0083a3e900
Gerrit Visscher <g.visscher@core4.de>
2015-10-14 10:35:51 +0200

    Fix install script for OSX, switch away from zsh

--- install/osx.sh
+++ install/osx.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-PACKAGES="tree imagemagick gnuplot nmap mtr git ack tmux unrar"
+PACKAGES=(tree imagemagick gnuplot nmap mtr git ack tmux unrar ruby fish)
 
 autoload -U colors && colors
 
@@ -19,50 +19,16 @@ else
 	read INSTALL_BREW
 	echo ""
 
-	if [[ "$INSTALL_OMZ" = "N" || "$INSTALL_OMZ" = "n" ]]; then
+	if [[ "$INSTALL_BREW" = "N" || "$INSTALL_BREW" = "n" ]]; then
 		echo "${WARNING} Skipping Homebrew..."
 	else
-		ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
-	fi
-
-fi
-
-# Check for oh my zsh
-if [[ -d $HOME/.oh-my-zsh ]]; then
-	echo "${STATUS} Found oh-my-zsh"
-else
-	# As first
-	echo -n "Install oh-my-zsh [Y/n]? "
-	read INSTALL_OMZ
-	echo ""
-
-	if [[ "$INSTALL_OMZ" = "N" || "$INSTALL_OMZ" = "n" ]]; then
-		echo "${WARNING} Skipping oh-my-zsh..."
-	else
-		echo "${STATUS} Installing oh-my-zsh..."
-		curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-	fi
-fi
-
-# Check if rvm is installed
-if [[ -d $HOME/.rvm ]]; then
-	echo "${STATUS} Found rvm."
-else
-	# As first
-	echo -n "Install rvm [Y/n]? "
-	read INSTALL_RVM
-	echo ""
-
-	if [[ "$INSTALL_RVM" = "N" || "$INSTALL_RVM" = "n" ]]; then
-		echo "${WARNING} Skipping rvm..."
-	else
-		echo "${STATUS} Installing rvm..."
-		curl -L https://get.rvm.io | bash -s stable
+		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 	fi
 
 fi
 
 # Install base packages
+echo "Base packages include: ${PACKAGES}"
 echo -n "Install base packages via brew [Y/n]? "
 read INSTALL_BASE
 
@@ -70,47 +36,35 @@ if [[ "$INSTALL_BASE" = "N" || "$INSTALL_BASE" = "n" ]]; then
 	echo "${WARNING} Skipping base packages..."
 else
 	echo "${STATUS} Installing base packages..."
-	/usr/local/bin/brew install $PACKAGES
+
+	for PACKAGE in $PACKAGES; do
+		/usr/local/bin/brew install $PACKAGE
+	done
 fi
 
 # Install symlinks
 cd $HOME
 
-if [[ -f $HOME/.zshrc ]]; then
-	echo "${WARNING} Moving old .zshrc to .zshrc-kayssun-bkp"
-	mv $HOME/.zshrc $HOME/.zshrc-kayssun-bkp
+FILE="$HOME/.config/fish/config.fish"
+if [[ -f $FILE ]]; then
+	echo "${WARNING} Moving old config.fish to config.fish-bkp"
+	mv $FILE ${FILE}-bkp
 fi
-if [[ -h $HOME/.zshrc ]]; then
-	rm $HOME/.zshrc
+if [[ -h $FILE ]]; then
+	rm $FILE
 fi
-ln -s .dotfiles/zshrc .zshrc
+ln -s .dotfiles/fish/config.fish .config/fish/config.fish
 
-if [[ -f $HOME/.vimrc ]]; then
-	echo "${WARNING} Moving old .vimrc to .vimrc-kayssun-bkp"
-	mv $HOME/.vimrc $HOME/.vimrc-kayssun-bkp
-fi
-if [[ -h $HOME/.vimrc ]]; then
-	rm $HOME/.vimrc
-fi
-ln -s .dotfiles/vimrc .vimrc
-
-if [[ -f $HOME/.gitconfig ]]; then
-	echo "${WARNING} Moving old .gitconfig to .gitconfig-kayssun-bkp"
-	mv $HOME/.gitconfig $HOME/.gitconfig-kayssun-bkp
-fi
-if [[ -h $HOME/.gitconfig ]]; then
-	rm $HOME/.gitconfig
-fi
-
-ln -s .dotfiles/gitconfig .gitconfig
-
-if [[ -f $HOME/.gitignore_global ]]; then
-	echo "${WARNING} Moving old .gitignore_global to .gitignore_global-kayssun-bkp"
-	mv $HOME/.gitignore_global $HOME/.gitignore_global-kayssun-bkp
-fi
-if [[ -h $HOME/.gitignore_global ]]; then
-	rm $HOME/.gitignore_global
-fi
-ln -s .dotfiles/gitignore_global .gitignore_global
+FILES=(vimrc gemrc pryrc gitignore_global)
+for FILE in $FILES; do
+	if [[ -f $HOME/.$FILE ]]; then
+		echo "${WARNING} Moving old .$FILE to .${FILE}-bkp"
+		mv "$HOME/.$FILE" "$HOME/.${FILE}-bkp"
+	fi
+	if [[ -h $HOME/.$FILE ]]; then
+		rm $HOME/.$FILE
+	fi
+	ln -s .dotfiles/$FILE .$FILE
+done
 
 echo "${STATUS} Done."
